1decb8126d755625016a8e9d8e6b890fce3301ad
Shaun Cutts <shaunc@factfiber.com>
2018-06-07 11:53:08 -0400

    fix index.js for non-coffee environment

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 // require coffee if possible; js otherwise
 try {
-  require('coffee-script/register');  
+  require('coffee-script/register');
   AsyncPool = require('./src/async-pool');
 }
 catch (e) {
-  if(e.message.indexOf("Cannot find module") != -1 
-      && (e.message.indexOf('./src/index') != -1 
-        || e.message.indexOf('coffee-script/register') != -1))
-    AsyncPool = require('./lib/async-pool');
-  else
-    throw e;
+  AsyncPool = require('./lib/async-pool')
 }
 module.exports = AsyncPool;
